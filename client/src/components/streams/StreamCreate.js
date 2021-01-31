b4e81338ddcb7a4af50ef108f5d256669a9570e8
Mahmoud Dawa <mahmoud.dawa@natuvion.com>
2021-01-31 21:12:49 +0100

    Initial commit

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {Field , reduxForm } from 'redux-form';
+
+class StreamCreate extends Component{
+  renderError({touched, error}){
+    if(touched && error){
+      return(
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      )
+    }
+  }
+  renderInput=({input, label, meta})=>{
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`
+      return (
+        <div className={className}>
+          <label>{label}</label>
+          <input {...input} autoComplete="off"/>
+          {this.renderError(meta)}
+      </div>
+      )}
+  onSubmit(event){
+    console.log(event);
+    //event.preventDefault();
+
+  }
+
+  render(){
+    
+    return (
+    <div>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    </div>);
+  }
+  
+};
+
+const validate = formValues =>{
+  const errors= {};
+  if(!formValues.title){
+    errors.title = "Enter a valid title"
+  }
+  if(!formValues.description){
+    errors.description = "Enter a valid Description"
+  }
+  return errors;
+}
+
+export default reduxForm({
+  form:'streamCreate',
+  validate
+})(StreamCreate);
